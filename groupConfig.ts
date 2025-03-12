dc4f3e768e13e79105f7d5b60fda636da35b6385
joy <NjAkACO9zibnrz5GEHNgSFO83zucNNcI@protonmail.com>
2025-03-12 01:26:21 +0000

    xd!

---
+++ groupConfig.ts
@@ -0,0 +1,45 @@
+
+interface Group {
+  name: string;
+  url: string;
+  logo: string;
+  role?: string;
+}
+
+export const groups: Group[] = [
+  {
+    name: "Central Medical Institute",
+    url: "https://www.roblox.com/communities/33504510/Central-Medical-Institute",
+    logo: "https://hr-network.xyz/assets/cmi-logo.png"
+  },
+  {
+    name: "Southlake Dentistry", 
+    url: "https://www.roblox.com/communities/34817411/Southlake-Dentistry",
+    logo: "https://hr-network.xyz/assets/southlake-logo.png"
+  },
+  {
+    name: "Northport Correctional",
+    url: "https://www.roblox.com/communities/35107354/Northport-Correctional",
+    logo: "https://hr-network.xyz/assets/northport-logo.png"
+  },
+  {
+    name: "Aerborne Airport Roleplay",
+    url: "https://www.roblox.com/communities/33205238/Aerborne-Airport-Roleplay",
+    logo: "https://hr-network.xyz/assets/aerborne-logo.png"
+  },
+  {
+    name: "Blau Hotels & Resorts",
+    url: "https://www.roblox.com/communities/4595505/Blau-Hotels-Resorts",
+    logo: "https://hr-network.xyz/assets/blau-logo.png"
+  },
+  {
+    name: "Seaborne Cruiselines Roleplay",
+    url: "https://www.roblox.com/communities/33862880/Seaborne-Cruiselines-Roleplay",
+    logo: "https://hr-network.xyz/assets/seaborne-logo.png"
+  },
+  {
+    name: "Restaurant Roleplay",
+    url: "https://www.roblox.com/communities/8927586/Restaurant-Roleplay",
+    logo: "https://hr-network.xyz/assets/rr-logo.png"
+  }
+];
